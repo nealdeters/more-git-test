0474fa96b659f1637dcca0b21373c0eb6234afbb
nealdeters <nealdeters@gmail.com>
2016-01-05 20:13:51 -0600

    excercise3_a updated

--- exercise3_a.rb
+++ exercise3_a.rb
@@ -1,7 +1,7 @@
 # Create a banking program that asks the user 5 times to enter a first name, last name, and email. This information should be stored as an array of hashes.
 users = {first_name: [], last_name: [], email: []}
 
-2.times do 
+5.times do 
   puts "Enter your first name: "
   first_name = gets.chomp
   users[:first_name] << first_name
